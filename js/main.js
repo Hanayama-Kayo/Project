4cc36d3b7952f6235113890aa4a64a1992ca1e9f
Hanayama-Kayo <testcomboom@gmail.com>
2023-04-27 18:34:03 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "light") {
+    this.firstElementChild.src = "images/dark.svg";
+  } else {
+    this.firstElementChild.src = "images/light.svg";
+  }
+  document.body.classList.toggle("light");
+});
+
+const loading = document.getElementById("loading");
+setTimeout(() => {
+  loading.classList.add('loading-none');
+}, 3000);
